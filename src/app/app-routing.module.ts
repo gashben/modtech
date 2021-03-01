f98d83aba4a92755591df5c5a05db362a94a04d2
Gashben <Gashben@DESKTOP-NHQKJGH>
2021-03-01 14:09:11 +0300

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ModtechComponent } from './modtech/modtech.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home',pathMatch:'full'},
+  {path: 'home' , component:HomeComponent,
+  children:[
+    {path: 'modtech' , component: ModtechComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routerComponent = [
+  HomeComponent,
+  ModtechComponent
+]
---
+++ src/app/modtech/modtech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modtech',
+  templateUrl: './modtech.component.html',
+  styleUrls: ['./modtech.component.css']
+})
+export class ModtechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
